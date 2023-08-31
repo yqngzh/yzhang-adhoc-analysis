ac15d797ca1a0cd725408749cbba021cd05afde0
Yuqing Zhang <yzhang@etsy.com>
2023-08-30 18:19:25 -0700

    Update process logic

--- 2023Q4_visual_diversity/analyze_embedding.py
+++ 2023Q4_visual_diversity/analyze_embedding.py
@@ -35,56 +35,56 @@ on train_data.listing_id = fb_data.listing_id
 order by requestUUID, position
 """
 
-# input_data = [
-#     {
-#         "requestUUID": "aaa",
-#         "position": 0,
-#         "query_str": "test1",
-#         "query_bin": "head",
-#         "listing_id": 902792488,
-#         "embedding": [{"element": 1.0}, {"element": 2.0}],
-#     },
-#     {
-#         "requestUUID": "aaa",
-#         "position": 1,
-#         "query_str": "test1",
-#         "query_bin": "head",
-#         "listing_id": 725104244,
-#         "embedding": [{"element": 3.0}, {"element": 4.0}],
-#     },
-#     {
-#         "requestUUID": "aaa",
-#         "position": 2,
-#         "query_str": "test1",
-#         "query_bin": "head",
-#         "listing_id": 1221932302,
-#         "embedding": [{"element": 5.0}, {"element": 6.0}],
-#     },
-#     {
-#         "requestUUID": "bbb",
-#         "position": 0,
-#         "query_str": "xxx",
-#         "query_bin": "top01",
-#         "listing_id": 793904541,
-#         "embedding": [{"element": 7.0}, {"element": 8.0}],
-#     },
-#     {
-#         "requestUUID": "bbb",
-#         "position": 1,
-#         "query_str": "xxx",
-#         "query_bin": "top01",
-#         "listing_id": 795642038,
-#         "embedding": None,
-#     },
-#     {
-#         "requestUUID": "bbb",
-#         "position": 2,
-#         "query_str": "xxx",
-#         "query_bin": "top01",
-#         "listing_id": 1323065344,
-#         "embedding": [],
-#     },
-# ]
+input_data = [
+    {
+        "requestUUID": "aaa",
+        "position": 1,
+        "query_str": "test1",
+        "query_bin": "head",
+        "listing_id": 725104244,
+        "embedding": [{"element": 3.0}, {"element": 4.0}],
+    },
+    {
+        "requestUUID": "aaa",
+        "position": 0,
+        "query_str": "test1",
+        "query_bin": "head",
+        "listing_id": 902792488,
+        "embedding": [{"element": 1.0}, {"element": 2.0}],
+    },
+    {
+        "requestUUID": "aaa",
+        "position": 2,
+        "query_str": "test1",
+        "query_bin": "head",
+        "listing_id": 1221932302,
+        "embedding": [{"element": 5.0}, {"element": 6.0}],
+    },
+    {
+        "requestUUID": "bbb",
+        "position": 0,
+        "query_str": "xxx",
+        "query_bin": "top01",
+        "listing_id": 793904541,
+        "embedding": [{"element": 7.0}, {"element": 8.0}],
+    },
+    {
+        "requestUUID": "bbb",
+        "position": 1,
+        "query_str": "xxx",
+        "query_bin": "top01",
+        "listing_id": 795642038,
+        "embedding": None,
+    },
+    {
+        "requestUUID": "bbb",
+        "position": 2,
+        "query_str": "xxx",
+        "query_bin": "top01",
+        "listing_id": 1323065344,
+        "embedding": [],
+    },
+]
 
 
 def run_query_df(query_str, project_id="etsy-bigquery-adhoc-prod"):
@@ -121,22 +121,27 @@ class GetEmbeddingAndSimilarity(beam.DoFn):
             "requestUUID": in_data[0]["requestUUID"],
             "query_str": in_data[0]["query_str"],
             "query_bin": in_data[0]["query_bin"],
-            "listing_ids": [],
         }
-        prev_position = -1
-        embeddings = []
+
+        new_listing_ids, positions, embeddings = [], [], []
         for i in range(len(in_data)):
             curr_in_data = in_data[i]
-            assert curr_in_data["position"] > prev_position
-            prev_position = curr_in_data["position"]
             if (curr_in_data["embedding"] is not None) and (
                 len(curr_in_data["embedding"]) > 0
             ):
-                out_data["listing_ids"].append(curr_in_data["listing_id"])
+                new_listing_ids.append(curr_in_data["listing_id"])
                 embeddings.append(
                     [item["element"] for item in curr_in_data["embedding"]]
                 )
-        out_data["cosine_sim"] = self.compute_similarity(embeddings)
+                positions.append(curr_in_data["position"])
+
+        # sort by position
+        zipped_data = sorted(zip(positions, new_listing_ids, embeddings))
+        new_listing_ids_sorted = [item[1] for item in zipped_data]
+        embeddings_sorted = [item[2] for item in zipped_data]
+
+        out_data["listing_ids"] = new_listing_ids_sorted
+        out_data["cosine_sim"] = self.compute_similarity(embeddings_sorted)
         return [out_data]
 
 
@@ -186,7 +191,7 @@ def run(argv=None):
                 gcs_location=f"{args.output_dir}/bq",
             )
             | "Group by request" >> beam.GroupBy(lambda x: x["requestUUID"])
-            | "Get embedding" >> beam.ParDo(GetEmbeddingAndSimilarity())
+            | "Get embedding similarity" >> beam.ParDo(GetEmbeddingAndSimilarity())
             # | "Print" >> beam.Map(print)
             | "Write results to BigQuery"
             >> beam.io.WriteToBigQuery(
