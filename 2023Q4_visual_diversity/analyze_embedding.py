80a2fe55975ee187ecaa5c709d3700ddeae2b460
Yuqing Zhang <yzhang@etsy.com>
2023-09-12 09:42:23 -0700

    Add visit ID to table

--- 2023Q4_visual_diversity/analyze_embedding.py
+++ 2023Q4_visual_diversity/analyze_embedding.py
@@ -9,72 +9,21 @@ from apache_beam.io.gcp.internal.clients.bigquery import TableSchema
 from apache_beam.io.gcp.internal.clients.bigquery import TableFieldSchema
 from google.cloud import bigquery
 
-input_data = [
-    {
-        "requestUUID": "aaa",
-        "position": 1,
-        "query_str": "test1",
-        "query_bin": "head",
-        "listing_id": 725104244,
-        "embedding": [{"element": 3.0}, {"element": 4.0}],
-    },
-    {
-        "requestUUID": "aaa",
-        "position": 0,
-        "query_str": "test1",
-        "query_bin": "head",
-        "listing_id": 902792488,
-        "embedding": [{"element": 1.0}, {"element": 2.0}],
-    },
-    {
-        "requestUUID": "aaa",
-        "position": 2,
-        "query_str": "test1",
-        "query_bin": "head",
-        "listing_id": 1221932302,
-        "embedding": [{"element": 5.0}, {"element": 6.0}],
-    },
-    {
-        "requestUUID": "bbb",
-        "position": 0,
-        "query_str": "xxx",
-        "query_bin": "top01",
-        "listing_id": 793904541,
-        "embedding": [{"element": 7.0}, {"element": 8.0}],
-    },
-    {
-        "requestUUID": "bbb",
-        "position": 1,
-        "query_str": "xxx",
-        "query_bin": "top01",
-        "listing_id": 795642038,
-        "embedding": None,
-    },
-    {
-        "requestUUID": "bbb",
-        "position": 2,
-        "query_str": "xxx",
-        "query_bin": "top01",
-        "listing_id": 1323065344,
-        "embedding": [],
-    },
-]
+
+# def run_query_df(query_str, project_id="etsy-bigquery-adhoc-prod"):
+#     client = bigquery.Client(project=project_id)
+#     query_job = client.query(query_str)
+#     rows = query_job.result()
+#     df = rows.to_dataframe()
+#     return df
 
 
-def run_query_df(query_str, project_id="etsy-bigquery-adhoc-prod"):
-    client = bigquery.Client(project=project_id)
-    query_job = client.query(query_str)
-    rows = query_job.result()
-    df = rows.to_dataframe()
-    return df
-
-
-def convert_bq_beam_schema(schema):
-    output = []
-    for item in schema:
-        item_dict = item.to_api_repr()
-        output.append(f"{item_dict['name']}:{item_dict['type']}")
-    return ",".join(output)
+# def convert_bq_beam_schema(schema):
+#     output = []
+#     for item in schema:
+#         item_dict = item.to_api_repr()
+#         output.append(f"{item_dict['name']}:{item_dict['type']}")
+#     return ",".join(output)
 
 
 class GetEmbeddingAndSimilarity(beam.DoFn):
@@ -93,6 +42,7 @@ class GetEmbeddingAndSimilarity(beam.DoFn):
         in_data = row[1]
         out_data = {
             "requestUUID": in_data[0]["requestUUID"],
+            "visitId": in_data[0]["visitId"],
             "query_str": in_data[0]["query_str"],
             "query_bin": in_data[0]["query_bin"],
         }
@@ -154,6 +104,12 @@ def run(argv=None):
     output_schema_beam.fields.append(field_schema)
 
     field_schema = TableFieldSchema()
+    field_schema.name = "visitId"
+    field_schema.type = "STRING"
+    field_schema.mode = "NULLABLE"
+    output_schema_beam.fields.append(field_schema)
+
+    field_schema = TableFieldSchema()
     field_schema.name = "query_str"
     field_schema.type = "STRING"
     field_schema.mode = "NULLABLE"
@@ -179,6 +135,7 @@ def run(argv=None):
 
     output_schema_bq = [
         bigquery.SchemaField("requestUUID", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("visitId", bigquery.enums.SqlTypeNames.STRING),
         bigquery.SchemaField("query_str", bigquery.enums.SqlTypeNames.STRING),
         bigquery.SchemaField("query_bin", bigquery.enums.SqlTypeNames.STRING),
         bigquery.SchemaField(
