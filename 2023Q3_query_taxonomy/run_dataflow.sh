861b55477a9efd8d6f9a22b5581b3bcd0e0e0193
Yuqing Zhang <yzhang@etsy.com>
2023-10-03 19:19:34 -0700

    last pass parameter tuning

--- 2023Q3_query_taxonomy/run_dataflow.sh
+++ 2023Q3_query_taxonomy/run_dataflow.sh
@@ -34,3 +34,22 @@ python -m distrib_match_opp_sizing \
     --disk_size_gb 200 \
     --num_workers 16 \
     --max_num_workers 32
+
+
+python -m last_pass_boost_parameter \
+    --input_table etsy-sr-etl-prod.yzhang.query_taxo_distrib_match_clean_2023_09_24 \
+    --output_table etsy-sr-etl-prod:yzhang.query_taxo_distrib_match_2023_09_24_summary \
+    --output_dir gs://etldata-prod-search-ranking-data-hkwv8r/data/shared/tmp \
+    --runner DataflowRunner \
+    --project etsy-sr-etl-prod \
+    --region us-central1 \
+    --service_account_email dataflow-worker@etsy-sr-etl-prod.iam.gserviceaccount.com \
+    --temp_location gs://etldata-prod-search-ranking-data-hkwv8r/data/shared/tmp \
+    --staging_location gs://etldata-prod-search-ranking-data-hkwv8r/data/shared/tmp \
+    --experiment use_runner_v2 \
+    --experiment upload_graph \
+    --experiment max_workflow_runtime_walltime_seconds=43200 \
+    --machine_type e2-standard-16 \
+    --disk_size_gb 200 \
+    --num_workers 16 \
+    --max_num_workers 32
