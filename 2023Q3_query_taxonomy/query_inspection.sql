57d659538fbd749b192bb97d5f5291274d3a94a2
Yuqing Zhang <yzhang@etsy.com>
2023-11-03 15:01:06 -0700

    query inspection sql

--- 2023Q3_query_taxonomy/query_inspection.sql
+++ 2023Q3_query_taxonomy/query_inspection.sql
@@ -7,8 +7,17 @@ order by queryLevelMetrics_gms desc
 limit 20
 
 select `key` as query,
+  queryTaxoDemandFeatures_impressionTopTaxonomyPaths,
+  queryTaxoDemandFeatures_impressionTopTaxonomyCounts
+  queryTaxoDemandFeatures_purchaseTopTaxonomyPaths,
+  queryTaxoDemandFeatures_purchaseTopTaxonomyCounts
+FROM `etsy-ml-systems-prod.feature_bank_v2.query_feature_bank_2023-10-31`
+where `key` = 'gift'
+
+select `key` as query,
+  queryTaxoDemandFeatures_impressionLevel2TaxonomyPaths,
+  queryTaxoDemandFeatures_impressionLevel2TaxonomyCounts
   queryTaxoDemandFeatures_purchaseLevel2TaxonomyPaths,
-  queryTaxoDemandFeatures_purchaseLevel2TaxonomyCounts,
-  queryTaxoDemandFeatures_purchaseLevel2TaxonomyPrices,
+  queryTaxoDemandFeatures_purchaseLevel2TaxonomyCounts
 FROM `etsy-ml-systems-prod.feature_bank_v2.query_feature_bank_2023-10-31`
 where `key` = 'gift'
