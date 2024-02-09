e780eddd119df957ec93854cde0c93fcf59fb897
Yuqing Zhang <yzhang@etsy.com>
2024-02-09 15:31:56 -0800

    expand common query

--- Useful_code/common_queries.sql
+++ Useful_code/common_queries.sql
@@ -81,6 +81,16 @@ left join user_data u
 on rpc_data.userId = u.user_id
 
 
+-- visit id beacon
+select 
+  (select value from unnest(beacon.properties.key_value) where key = 'query') as query,
+  (select value from unnest(beacon.properties.key_value) where key = 'translated_query') as translated_query,
+from `etsy-visit-pipe-prod.canonical.visit_id_beacons`
+where _PARTITIONTIME BETWEEN TIMESTAMP('2024-02-08') AND TIMESTAMP('2024-02-09')
+and beacon.event_name = "search"
+and (select value from unnest(beacon.properties.key_value) where key = 'translated_query') is null
+
+
 CREATE OR REPLACE EXTERNAL TABLE `etsy-sr-etl-prod.yzhang.query_missing_fl_raw`
 OPTIONS (
     format = 'parquet',
