1b099d3b791ea64b6abee6eb5753e5d6d1037aff
Yuqing Zhang <yzhang@etsy.com>
2024-05-25 12:32:20 -0700

    add price changes

--- 2024Q1_semantic_relevance/v1_pw_tire_analysis.sql
+++ 2024Q1_semantic_relevance/v1_pw_tire_analysis.sql
@@ -1,4 +1,4 @@
-create or replace table `etsy-sr-etl-prod.yzhang.sem-rel-pw-tire-boe-log1p` as (
+create or replace table `etsy-sr-etl-prod.yzhang.sem-rel-pw-tire-web-pow0306` as (
     with tire_requests as (
         select
             response.mmxRequestUUID as tireRequestUUID,
@@ -12,7 +12,7 @@ create or replace table `etsy-sr-etl-prod.yzhang.sem-rel-pw-tire-boe-log1p` as (
             pos as tire_position
         from `etsy-searchinfra-gke-dev.thrift_tire_listingsv2search_search.rpc_logs_*`,
             UNNEST(response.solrScores) as solrScore WITH OFFSET pos
-        where tireRequestContext.tireTestv2Id = "kjHjQK89FadtyK5ucWEa"
+        where tireRequestContext.tireTestv2Id = "wTsd7S3O5mAIkSeNaBXd"
         and request.query != ""
         and response.mmxRequestUUID is not null
         and request.limit != 0
@@ -42,7 +42,7 @@ create or replace table `etsy-sr-etl-prod.yzhang.sem-rel-pw-tire-boe-log1p` as (
         count(*) AS count_uuid
     from rpc_segment
     group by tireRequestUUID, variant, page_no, buyer_segment, query_bin
-)
+);
 
 
 with grouped as (
@@ -52,7 +52,7 @@ with grouped as (
         variant,
         SUM(count_uuid) AS num_request,
         AVG(avg_price_per_uuid) as avg_price,
-    from `etsy-sr-etl-prod.yzhang.sem-rel-pw-tire-boe-log1p`
+    from `etsy-sr-etl-prod.yzhang.sem-rel-pw-tire-web-pow0306`
     where page_no = 1
     group by variant, segment, page_no
 )
@@ -66,7 +66,7 @@ with grouped as (
         variant,
         SUM(count_uuid) AS num_request,
         AVG(avg_price_per_uuid) as avg_price,
-    from `etsy-sr-etl-prod.yzhang.sem-rel-pw-tire-boe-log1p`
+    from `etsy-sr-etl-prod.yzhang.sem-rel-pw-tire-web-pow0306`
     where page_no = 1
     group by variant, page_no
 )
@@ -76,3 +76,23 @@ order by variant
 
 
 -- boe online impression price change
+with exp_data as (
+    select variant_id, listingId, 
+    from `etsy-data-warehouse-dev.search.sr-sem-rel-v1-boe_query-listing-metrics_vw`
+    where pageNum = 1
+),
+listing_price as (
+    select key as listingId, activeListingBasics_priceUsd as priceUsd
+    from `etsy-ml-systems-prod.feature_bank_v2.listing_feature_bank_most_recent`
+),
+joined as (
+    select e.variant_id, e.listingId, priceUsd
+    from exp_data e
+    left join listing_price lp
+    on e.listingId = lp.listingId
+)
+select variant_id, avg(priceUsd) as avg_price
+from joined
+group by variant_id
+-- control 53.998836
+-- test 54.067742
