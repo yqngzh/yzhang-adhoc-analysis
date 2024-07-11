145020a22d4f8bf18aac65544a5024818c98807e
Yuqing Zhang <yzhang@etsy.com>
2024-07-11 13:51:31 -0700

    update motivation code

--- 2024Q1_semantic_relevance/V2_ranking_motivation.sql
+++ 2024Q1_semantic_relevance/V2_ranking_motivation.sql
@@ -1,3 +1,5 @@
+------  All sampled search requests
+------ 
 with sampled_relevance_pred as (
     select 
         reqs.guid,
@@ -12,7 +14,7 @@ with sampled_relevance_pred as (
     from `etsy-data-warehouse-prod.search.sem_rel_query_listing_metrics` metrics
     join `etsy-data-warehouse-prod.search.sem_rel_hydrated_daily_requests` reqs
     using (tableUUID)
-    where reqs.date = date('2024-06-20')
+    where reqs.date = date('2024-07-08')
     and reqs.pageNum is not null
 ),
 web_top3page_reqs as (
@@ -35,6 +37,13 @@ mweb_top1page_reqs as (
 mweb_top2row_reqs as (
     select * from mweb_top1page_reqs where rankingRank between 0 and 3
 ),
+web_total_top3page_reqs as (
+    select * from sampled_relevance_pred
+    where pageNum is not null and platform in ('web', 'mweb')
+),
+web_total_top1page_reqs as (
+    select * from web_total_top3page_reqs where pageNum = 1
+),
 boe_top3page_reqs as (
     select * from sampled_relevance_pred
     where pageNum is not null and platform = 'boe'
@@ -45,40 +54,142 @@ boe_top1page_reqs as (
 boe_top2row_reqs as (
     select * from boe_top1page_reqs where rankingRank between 0 and 3
 )
-select count(*) from web_top3page_reqs
-where classId = 1
+select count(*) from web_top1page_reqs
+where qisClass = 'broad'
+and classId = 1
+
+-- segment by intent specificity
+select qisClass, classId, count(*) from web_total_top1page_reqs
+group by qisClass, classId
+order by qisClass, classId
 
 
------- irrelevance percentage (where classId = 1)
--- web 
----- top 3 page: 12601 / 96602 = 13.04%
----- top 1 page: 8112 / 68115 = 11.91%
----- top 2 rows: 966 / 11329 = 8.53%
+-- QIS distribution in sampled traffic
+with web_events_qis as (
+    select distinct
+        reqs.guid,
+        reqs.query,
+        qisClass,
+    from `etsy-data-warehouse-prod.search.sem_rel_query_listing_metrics` metrics
+    join `etsy-data-warehouse-prod.search.sem_rel_hydrated_daily_requests` reqs
+    using (tableUUID)
+    where reqs.date = date('2024-07-08')
+    and reqs.pageNum is not null
+    and platform in ('web', 'mweb')
+)
+select qisClass, count(*) as cnt
+from web_events_qis
+group by qisClass
 
--- mweb
----- top 3 page: 13040 / 91688 = 14.22%
----- top 1 page: 7353 / 57713 = 12.74%
----- top 2 rows: 614 / 6836 = 8.98%
 
--- boe
----- top 3 page: 20368 / 115798 = 17.59%
----- top 1 page: 10734 / 67524 = 15.90%
----- top 2 rows: 1197 / 9649 = 12.41%
+--- QIS attributed GMS past 3 months
+with qsn as (
+    select query, sum(attributed_gms) as attributed_gms
+    from `etsy-data-warehouse-prod.search.query_sessions_new`
+    where _date between ('2024-04-08') and ('2024-07-08')
+    group by query
+),
+qis as (
+    select 
+        query_raw query,
+        CASE 
+            WHEN class_id = 0 THEN 'broad' 
+            WHEN class_id = 1 THEN 'direct_unspecified'
+            WHEN class_id = 2 THEN 'direct_specified'
+        END AS qisClass
+    from `etsy-search-ml-prod.mission_understanding.qis_scores`
+),
+merged as (
+    select qsn.query, attributed_gms, qisClass
+    from qsn
+    left join qis
+    using (query)
+)
+select qisClass, sum(attributed_gms) as total_attributed_gms
+from merged
+group by qisClass
 
 
+--- queries with historical purchase in past 100 days
+with qsn as (
+    select query, max(has_purchase) as has_purchase
+    from `etsy-data-warehouse-prod.search.query_sessions_new`
+    where _date between ('2024-04-01') and ('2024-07-08')
+    group by query
+),
+sampled_relevance_pred as (
+    select 
+        reqs.guid,
+        reqs.query,
+        reqs.listingId,
+        queryBin,
+        qisClass,
+        platform,
+        reqs.pageNum,
+        reqs.rankingRank,
+        metrics.classId
+    from `etsy-data-warehouse-prod.search.sem_rel_query_listing_metrics` metrics
+    join `etsy-data-warehouse-prod.search.sem_rel_hydrated_daily_requests` reqs
+    using (tableUUID)
+    where reqs.date = date('2024-07-08')
+    and reqs.pageNum is not null
+),
+sampled_relevance_pred_with_purchase as (
+    select srp.*, has_purchase
+    from sampled_relevance_pred srp
+    left join qsn
+    using (query)
+),
+web_total_top3page_reqs as (
+    select * from sampled_relevance_pred_with_purchase
+    where pageNum is not null and platform in ('web', 'mweb')
+),
+web_total_top1page_reqs as (
+    select * from web_total_top3page_reqs where pageNum = 1
+),
+boe_top3page_reqs as (
+    select * from sampled_relevance_pred_with_purchase
+    where pageNum is not null and platform = 'boe'
+),
+boe_top1page_reqs as (
+    select * from boe_top3page_reqs where pageNum = 1
+)
+select has_purchase, classId, count(*) from web_total_top1page_reqs
+group by has_purchase, classId
+order by has_purchase, classId
 
------- not relevant percentage (where classId != 4)
--- web
----- top 3 page: 19064 / 96602 = 19.73%
----- top 1 page: 12410 / 68115 = 18.22%
----- top 2 rows: 1563 / 11329 = 13.80%
 
--- mweb
----- top 3 page: 18226 / 91688 = 19.88%
----- top 1 page: 10319 / 57713 = 17.88%
----- top 2 rows: 878 / 6836 = 12.84%
+-- queries with and without purchase, percent in sampled traffic
+with qsn as (
+    select query, max(has_purchase) as has_purchase
+    from `etsy-data-warehouse-prod.search.query_sessions_new`
+    where _date between ('2024-04-01') and ('2024-07-08')
+    group by query
+),
+web_reqs as (
+    select distinct
+        reqs.guid,
+        reqs.query,
+        -- qisClass
+    from `etsy-data-warehouse-prod.search.sem_rel_query_listing_metrics` metrics
+    join `etsy-data-warehouse-prod.search.sem_rel_hydrated_daily_requests` reqs
+    using (tableUUID)
+    where reqs.date = date('2024-07-08')
+    and reqs.pageNum is not null
+    and platform in ('web', 'mweb')
+),
+web_reqs_purchase as (
+    select web_reqs.*, has_purchase
+    from web_reqs
+    left join qsn
+    using (query)
+)
+select has_purchase, count(*) as cnt
+from web_reqs_purchase
+group by has_purchase
 
--- boe
----- top 3 page: 27407 / 115798 = 23.67%
----- top 1 page: 14473 / 67524 = 21.43%
----- top 2 rows: 1641 / 9649 = 17.01%
+select qisClass, has_purchase, count(*) as cnt
+from web_reqs_purchase
+group by qisClass, has_purchase
+order by qisClass, has_purchase
+
