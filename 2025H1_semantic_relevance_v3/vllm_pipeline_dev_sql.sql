b93a483b7c502cb2ecd7474b3aeea3cf539c2ebc
Yuqing Zhang <yzhang@etsy.com>
2025-04-29 18:56:52 -0700

    add sanity check sql

--- 2025H1_semantic_relevance_v3/vllm_pipeline_dev_sql.sql
+++ 2025H1_semantic_relevance_v3/vllm_pipeline_dev_sql.sql
@@ -11,3 +11,42 @@ CREATE OR REPLACE TABLE `etsy-search-ml-dev.search.yzhang_semrel_test` AS (
     LEFT JOIN lfb USING (listingId)
     WHERE date BETWEEN DATE('2025-04-01') AND DATE('2025-04-10')
 );
+
+
+
+--- Sanity check pipeline implementation
+WITH lfb AS (
+    SELECT
+        KEY listingId,
+        (SELECT STRING_AGG(element, ', ') FROM UNNEST(descNgrams_ngrams.list)) listingDescNgrams,
+        IFNULL(listingLlmFeatures_llmHeroImageDescription, "") listingHeroImageCaption,
+    FROM `etsy-ml-systems-prod.feature_bank_v2.listing_feature_bank_2025-04-24`
+), 
+merged AS (
+    SELECT *
+    FROM `etsy-data-warehouse-prod.search.sem_rel_hydrated_daily_requests_per_experiment`
+    LEFT JOIN lfb USING (listingId)
+    WHERE date = "2025-04-24"
+)
+SELECT count(*)
+FROM merged
+-- 13949394 rows, same as dataflow job
+-- 181139 distinct guid
+
+select count(*)
+from `etsy-search-ml-dev.yzhang.semrel_llm_test_query_listing_metrics`
+where date = "2025-04-24"
+and modelName = "v3-finetuned-llama-8b"
+-- 13949394 rows
+
+select count(*)
+from `etsy-search-ml-dev.yzhang.semrel_llm_test_request_metrics`
+where date = "2025-04-24"
+and modelName = "v3-finetuned-llama-8b"
+-- 181138 rows
+
+
+SELECT avg(relevanceNDCG4) 
+FROM `etsy-data-warehouse-prod.search.sem_rel_requests_metrics_per_experiment` 
+-- FROM  `etsy-search-ml-dev.yzhang.semrel_llm_test_request_metrics`
+WHERE date = "2025-04-24" 
--- diff_python.py
+++ diff_python.py
@@ -1,81 +1,177 @@
-class VLLMCompletionsModelHandler(ModelHandler[str,
-                                               PredictionResult,
-                                               _VLLMModelServer]):
-  def __init__(
-      self,
-      model_name: str,
-      vllm_server_kwargs: Optional[Dict[str, str]] = None):
-    """Implementation of the ModelHandler interface for vLLM using text as
-    input.
+import datetime
+from airflow import DAG
+from etsy.operators.gke import GKEDataflowPythonLaunch
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryExecuteQueryOperator,
+)
+from etsy.utils.defaults import patch_all
 
-    Example Usage::
+patch_all()
 
-      pcoll | RunInference(VLLMModelHandler(model_name='facebook/opt-125m'))
+DEFAULT_TASK_ARGS = {
+    "sla": datetime.timedelta(days=1),
+    "owner": "search",
+    "retries": 2,
+    "retry_delay": datetime.timedelta(seconds=300),
+    "start_date": datetime.datetime(2024, 8, 2, 0, 0),
+    "dag_monitor_email": [
+        "jgammack@etsy.com",
+        "yzhang@etsy.com",
+        "ebenjamin@etsy.com",
+        "aclapp@etsy.com",
+        "brussell@etsy.com",
+        "search-ranking-team@etsy.pagerduty.com",  # pls ping @search-semantic-relevance-engs in #search-semantic-relevance
+    ],
+}
 
-    Args:
-      model_name: The vLLM model. See
-        https://docs.vllm.ai/en/latest/models/supported_models.html for
-        supported models.
-      vllm_server_kwargs: Any additional kwargs to be passed into your vllm
-        server when it is being created. Will be invoked using
-        `python -m vllm.entrypoints.openai.api_serverv <beam provided args>
-        <vllm_server_kwargs>`. For example, you could pass
-        `{'echo': 'true'}` to prepend new messages with the previous message.
-        For a list of possible kwargs, see
-        https://docs.vllm.ai/en/latest/serving/openai_compatible_server.html#extra-parameters-for-completions-api
-    """
-    self._model_name = model_name
-    self._vllm_server_kwargs: Dict[str, str] = vllm_server_kwargs or {}
-    self._env_vars = {}
+with DAG(
+    dag_id="semantic_relevance_baseline",
+    catchup=True,
+    max_active_runs=4,
+    max_active_tasks=4,
+    schedule="@daily",
+    default_args=DEFAULT_TASK_ARGS,
+) as dag:
+    sampling_table = "etsy-data-warehouse-prod.search.sem_rel_hydrated_daily_requests"
 
-  def load_model(self) -> _VLLMModelServer:
-    return _VLLMModelServer(self._model_name, self._vllm_server_kwargs)
+    inference_result_table_prefix = ""
+    inference_output_pairs_table_name = (
+        "etsy-data-warehouse-prod.search.sem_rel_query_listing_metrics"
+    )
+    inference_output_requests_table_name = (
+        "etsy-data-warehouse-prod.search.sem_rel_requests_metrics"
+    )
+    # if inference_result_table_prefix is not ""
+    # inference results will be written to
+    # pairs table: [inference_result_table_prefix]_query_listing_metrics(_vw), and
+    # request table: [inference_result_table_prefix]_request_metrics(_vw)
+    # otherwise, it will use the provided table names
 
-  async def _async_run_inference(
-      self,
-      batch: Sequence[str],
-      model: _VLLMModelServer,
-      inference_args: Optional[Dict[str, Any]] = None
-  ) -> Iterable[PredictionResult]:
-    client = getAsyncVLLMClient(model.get_server_port())
-    inference_args = inference_args or {}
-    async_predictions = []
-    for prompt in batch:
-      try:
-        completion = client.completions.create(
-            model=self._model_name, prompt=prompt, **inference_args)
-        async_predictions.append(completion)
-      except Exception as e:
-        model.check_connectivity()
-        raise e
+    ##  Sample requests and write data to BigQuery
+    sample_search_requests_for_inference = BigQueryExecuteQueryOperator(
+        task_id="sample_search_requests_for_inference",
+        sql="""
+        SELECT * FROM `etsy-data-warehouse-prod.search.sem_rel_get_requests`('{{ macros.etsy.hyphenated_days_ago(1, ds) }}', 88, 0.0015, 'web', 48, 16)
+        UNION ALL
+        SELECT * FROM `etsy-data-warehouse-prod.search.sem_rel_get_requests`('{{ macros.etsy.hyphenated_days_ago(1, ds) }}', 74, 0.0015, 'mweb', 34, 15)
+        UNION ALL
+        SELECT * FROM `etsy-data-warehouse-prod.search.sem_rel_get_requests`('{{ macros.etsy.hyphenated_days_ago(1, ds) }}', 68, 0.0015, 'boe', 28, 9)
+        """,
+        destination_dataset_table=sampling_table,
+        write_disposition="WRITE_APPEND",
+        create_disposition="CREATE_IF_NEEDED",
+        time_partitioning={"field": "date", "type": "DAY"},
+        use_legacy_sql=False,
+        maximum_bytes_billed=36000000000000.0,
+        location="US",
+    )
 
-    predictions = []
-    for p in async_predictions:
-      try:
-        predictions.append(await p)
-      except Exception as e:
-        model.check_connectivity()
-        raise e
+    ##  Semantic relevance model inference
+    timestamp = datetime.datetime.utcnow().strftime("%Y%m%d-%H%M%S")
+    job_name_prefix = "sem-rel-baseline-dashboard"
+    project_id = "{{ macros.etsy.test_switch(in_test='etsy-search-ml-dev', in_prod='etsy-search-ml-prod') }}"
+    SERVICE_ACCOUNT_NAME = "{{ macros.etsy.test_switch(in_test='airflow-search-aiplatform-dev', in_prod='airflow-search-aiplatform-prod') }}"
 
-    return [PredictionResult(x, y) for x, y in zip(batch, predictions)]
+    model_args_dicts = [
+        # V1
+        {
+            "model_type": "v1",
+            "model_name": "bert-cern-l24-h1024-a16",
+            "model_path": "gs://training-dev-search-data-jtzn/semantic_relevance/production_models/bert-cern-l24-h1024-a16-v1/export/saved_model",
+            "tag": "v2.2.7",
+            "num_workers": "10",
+            "batch_size": "128",
+        },
+        # V2
+        {
+            "model_type": "v2",
+            "model_name": "v2-deberta-v3-large-tad",
+            "model_path": "gs://training-dev-search-data-jtzn/semantic_relevance/production_models/v2-deberta-v3-large-tad/export/saved_model",
+            "tag": "v2.2.7",
+            "num_workers": "32",
+            "batch_size": "8",
+        },
+    ]
 
-  def run_inference(
-      self,
-      batch: Sequence[str],
-      model: _VLLMModelServer,
-      inference_args: Optional[Dict[str, Any]] = None
-  ) -> Iterable[PredictionResult]:
-    """Runs inferences on a batch of text strings.
+    for model_args in model_args_dicts:
+        model_type = model_args["model_type"]
+        model_name = model_args["model_name"]
+        model_path = model_args["model_path"]
+        tag = model_args["tag"]
+        num_workers = model_args["num_workers"]
+        batch_size = model_args["batch_size"]
 
-    Args:
-      batch: A sequence of examples as text strings.
-      model: A _VLLMModelServer containing info for connecting to the server.
-      inference_args: Any additional arguments for an inference.
+        job_name = f"{job_name_prefix}-{model_name}"
+        job_id = f"{job_name}-{timestamp}"
 
-    Returns:
-      An Iterable of type PredictionResult.
-    """
-    return asyncio.run(self._async_run_inference(batch, model, inference_args))
+        image = (
+            f"gcr.io/etsy-gcr-dev/ml-infra/vertex/pipelines/semantic_relevance:{tag}"
+        )
 
-  def share_model_across_processes(self) -> bool:
-    return True
+        dataflow_options = [
+            "--runner",
+            "DataflowRunner",
+            "--project",
+            project_id,
+            "--region",
+            "us-east1",
+            "--service_account_email",
+            f"sa-aiplatform@{project_id}.iam.gserviceaccount.com",
+            "--num_workers",
+            "1",
+            "--max_num_workers",
+            num_workers,
+            "--flexrs_goal",
+            "SPEED_OPTIMIZED",
+            "--machine_type",
+            "g2-standard-4",
+            "--dataflow_service_options",
+            "worker_accelerator=type:nvidia-l4;count:1;install-nvidia-driver",
+            "--number_of_worker_harness_threads",
+            "1",
+            "--sdk_container_image",
+            image,
+            "--experiment",
+            "max_workflow_runtime_walltime_seconds=21600",
+        ]
+
+        dataflow_gpu_inference_runner = GKEDataflowPythonLaunch(
+            task_id=f"run_semrel_inference_{model_name}",
+            python_module="semantic_relevance.evaluation.bigquery_metrics",
+            image=image,
+            service_account_name=SERVICE_ACCOUNT_NAME,
+            labels={
+                "owner": "search-relevance",
+                "pipeline_name": "semantic-relevance-bigquery-metrics",
+                "job_trigger": "airflow",
+                "job_name": job_name,
+            },
+            arguments=[
+                "--job_name",
+                job_id,
+                "--date",
+                "{{ macros.etsy.hyphenated_days_ago(1, ds) }}",
+                "--input_table",
+                sampling_table,
+                "--model_type",
+                model_type,
+                "--model_name",
+                model_name,
+                "--model_path",
+                model_path,
+                "--bigquery_project_id",
+                project_id,
+                "--output_table_prefix",
+                inference_result_table_prefix,
+                "--pairs_table_name",
+                inference_output_pairs_table_name,
+                "--requests_table_name",
+                inference_output_requests_table_name,
+                "--batch_size",
+                batch_size,
+            ]
+            + dataflow_options,
+        )
+
+        ##  Construct Airflow DAG
+        sample_search_requests_for_inference >> dataflow_gpu_inference_runner
