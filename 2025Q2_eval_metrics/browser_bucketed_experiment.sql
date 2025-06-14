12c938b217581ff2f3dfaad6201710d51ff7962f
Yuqing Zhang <yzhang@etsy.com>
2025-06-14 10:33:26 -0700

    more attempts to match n requests

--- 2025Q2_eval_metrics/browser_bucketed_experiment.sql
+++ 2025Q2_eval_metrics/browser_bucketed_experiment.sql
@@ -69,7 +69,6 @@ create or replace table `etsy-search-ml-dev.yzhang.unified_try2_offline` as (
     on (
         offres.browserId = bbp.bucketing_id
         and offres.event_timestamp >= bbp.bucketing_ts
-        -- same number with and without timestamp filtering
         -- and TIMESTAMP(offres.evalDate) >= bbp.boundary_start_ts
     )
 )
@@ -108,6 +107,8 @@ order by variant_id
 -- off                | 33878                      | 41317
 -- on                 | 33279                      | 41090
 
+
+--- Get pNDCG anyways
 select 
     modelName,
     avg(metrics.purchase.ndcg48) as avg_pndcg48, 
@@ -145,7 +146,8 @@ join pndcg_table using (variant_id)
 
 
 
----- Try only checking number of requests per day
+---- Try only checking number of requests per day, not thinking about variants
+-- offline
 with requests_info as (
     select distinct 
         requestUUID,
@@ -191,6 +193,53 @@ on (
 -- offline no event time check 88461
 -- offline with event time check 67157
 
+-- offline try bucketing table instead of bucketingPeriod
+with requests_info as (
+    select distinct 
+        requestUUID,
+        TIMESTAMP_MILLIS(timeSinceEpochMs) as event_timestamp,
+        SPLIT(visitId, ".")[OFFSET(0)] as browserId,
+    from `etsy-ml-systems-prod.attributed_instance.query_pipeline_web_organic_tight_2025_05_30`
+    where "purchase" in unnest(attributions)
+    union all
+    select distinct 
+        requestUUID,
+        TIMESTAMP_MILLIS(timeSinceEpochMs) as event_timestamp,
+        SPLIT(visitId, ".")[OFFSET(0)] as browserId
+    from `etsy-ml-systems-prod.attributed_instance.query_pipeline_boe_organic_tight_2025_05_30`
+    where "purchase" in unnest(attributions)
+),
+requests_info_clean as (
+    select distinct requestUUID, event_timestamp, browserId
+    from requests_info
+    where event_timestamp is not null
+    and browserId is not null
+),
+firstBucketing as (
+    SELECT
+        bucketing_id,
+        variant_id,
+        MIN(bucketing_ts) AS bucketing_ts,
+    FROM `etsy-data-warehouse-prod.catapult_unified.bucketing`
+    -- where _date = "2025-05-30"
+    WHERE _date between "2025-05-23" and "2025-05-30"
+    and experiment_id = "ranking/search.mmx.2025_q2.nrv2_unified_ranking_try2"
+    GROUP BY bucketing_id, variant_id
+)
+select count(distinct requestUUID)
+from requests_info_clean offres
+join firstBucketing bbp
+on (
+    offres.browserId = bbp.bucketing_id
+    and offres.event_timestamp >= bbp.bucketing_ts
+)
+-- date = 0530 + time check: 53190
+-- date = 0530: 88444
+---- equivalent to using bucketing_period
+-- date between start and 0530 + time check: 67157
+-- date between start and 0530: 88461
+
+-- online
 with catapult_data as (
     SELECT
         _date,
