822fff44d82939fe5761436edcda5b1a81177705
Yuqing Zhang <yzhang@etsy.com>
2025-08-26 14:03:55 -0700

    us sign-out

--- 2025H2_exact_match/find_queries/find_queries.sql
+++ 2025H2_exact_match/find_queries/find_queries.sql
@@ -1,5 +1,27 @@
-create or replace table `etsy-search-ml-dev.search.yzhang_emqueries_try1` as (
-    with semrel_teacher_results as (
+declare start_date date default "2025-07-25";
+declare end_date date default "2025-08-25";
+
+create or replace table `etsy-search-ml-dev.search.yzhang_emqueries_step1` as (
+    with allrpc as (
+        SELECT
+            response.mmxRequestUUID as mmxRequestUUID,
+            COALESCE((SELECT NULLIF(query, '') FROM UNNEST(request.filter.query.translations) WHERE language = 'en'), NULLIF(request.query, '')) query,
+            DATE(queryTime) as date,
+            IF(request.options.personalizationOptions.userId > 0, "SI", "SO") si_so
+        FROM `etsy-searchinfra-gke-prod-2.thrift_mmx_listingsv2search_search.rpc_logs_*`
+        WHERE request.OPTIONS.cacheBucketId LIKE "live%"
+        AND request.options.csrOrganic
+        AND request.options.searchPlacement IN ('wsg', 'allsr', 'wmg')
+        AND request.query <> ''
+        AND request.offset = 0
+        AND DATE(queryTime) between start_date and end_date
+        AND request.options.interleavingConfig IS NULL
+        AND NOT EXISTS (
+            SELECT * FROM UNNEST(request.context)
+            WHERE key = "req_source" AND value = "bot"
+        )
+    ),
+    semrel_teacher_page1 as (
         SELECT
             mmxRequestUUID,
             guid,
@@ -19,9 +41,17 @@ create or replace table `etsy-search-ml-dev.search.yzhang_emqueries_try1` as (
         FROM `etsy-data-warehouse-prod.search.sem_rel_hydrated_daily_requests` r
         JOIN `etsy-data-warehouse-prod.search.sem_rel_query_listing_metrics` USING (tableUUID)
         WHERE modelName = "v3-finetuned-llama-8b" 
-        AND r.date between "2025-07-22" and "2025-08-22"
+        AND r.date between start_date and end_date
         AND pageNum = 1
         AND r.resultType = "organic"
+    ),
+    selected_data as (
+        select sr.*
+        from semrel_teacher_page1 sr
+        left join allrpc using (mmxRequestUUID, query, date)
+        where si_so = "SO"
+        and userLanguage = "en-US"
+        and userCountry = "US"
     ),
     agg_requests as (
         select 
@@ -30,15 +60,13 @@ create or replace table `etsy-search-ml-dev.search.yzhang_emqueries_try1` as (
             query, 
             date,
             platform, 
-            userLanguage,
-            userCountry,
             count(*) as n_total, 
             sum(if(semrelClass = 'Relevant', 1, 0)) as n_em,
             array_agg(listingId order by rankingRank asc) as listingIdArray,
             array_agg(semrelClass order by rankingRank asc) as semrelClassArray,
             array_agg(rankingRank order by rankingRank asc) as rankingRankArray,
-        from semrel_teacher_results
-        group by mmxRequestUUID, guid, date, query, platform, userLanguage, userCountry
+        from selected_data
+        group by mmxRequestUUID, guid, date, query, platform
     ),
     valid_requests as (
         select * from agg_requests
@@ -59,14 +87,5 @@ create or replace table `etsy-search-ml-dev.search.yzhang_emqueries_try1` as (
     select req.*, queryBin 
     from valid_requests_w_pct_em req
     left join qlm using (query)
-    where queryBin in ("top.01", "top.1", "tail")
-    and pct_em <= 0.6
+    where pct_em <= 0.6
 )
-
-select distinct query, date, queryBin, pct_em
-from `etsy-search-ml-dev.search.yzhang_emqueries_try1`
-where platform = "web"
-and userLanguage = "en-US"
-and userCountry = "US"
-and date = "2025-07-22"
-order by queryBin desc
