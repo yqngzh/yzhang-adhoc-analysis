aada055aa57a6c11ede5fe0bb6f7ccb176e78cbd
Yuqing Zhang <yzhang@etsy.com>
2025-09-12 14:50:21 -0700

    update analysis code

--- Useful_code/common_queries/candidate_set_stats_from_stages.sql
+++ Useful_code/common_queries/candidate_set_stats_from_stages.sql
@@ -7,62 +7,7 @@ WITH rpc AS (
         response.semanticRelevanceModelInfo.modelSetName as sem_rel_modelset_name,
         response.semanticRelevanceScores AS semanticRelevanceScores
     FROM `etsy-searchinfra-gke-prod-2.thrift_mmx_listingsv2search_search.rpc_logs_*`
-    WHERE queryTime BETWEEN TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 7 DAY) AND CURRENT_TIMESTAMP()
-    AND request.options.cacheBucketId LIKE 'live%'
-    AND request.query <> ''
-    AND request.options.csrOrganic
-    AND request.options.searchPlacement IN ('wsg', "wmg", "allsr")
-    AND request.offset = 0
-    AND request.options.interleavingConfig IS NULL
-    AND OrganicRequestMetadata IS NOT NULL
-    AND EXISTS (
-        SELECT 1 FROM UNNEST(OrganicRequestMetadata.candidateSources) cs
-        WHERE cs.stage IN ('POST_FILTER', 'POST_SEM_REL_FILTER', 'POST_BORDA', 'RANKING', 'MO_LASTPASS')
-            AND cs.listingIds IS NOT NULL
-            AND ARRAY_LENGTH(cs.listingIds) > 0
-    )
-    AND NOT EXISTS (
-        SELECT 1 FROM UNNEST(request.context)
-        WHERE key = 'req_source' AND value = 'bot'
-    )
-    AND response.mmxRequestUUID IS NOT NULL
-),
-
-stage_counts AS (
-    SELECT
-        rpc.mmxRequestUUID,
-        rpc.query,
-        rpc.queryDate,
-        rpc.sem_rel_modelset_name,
-        cs.stage,
-        ARRAY_LENGTH(cs.listingIds) AS candidate_count,
-    FROM rpc, UNNEST(rpc.candidateSources) cs
-    WHERE cs.stage IN ('POST_FILTER', 'POST_SEM_REL_FILTER', 'POST_BORDA', 'RANKING', 'MO_LASTPASS')
-    AND cs.listingIds IS NOT NULL
-    AND ARRAY_LENGTH(cs.listingIds) > 0
-),
-
-semrel_filtered_count as (
-    select 
-        mmxRequestUUID,
-        query,
-        queryDate,
-        SUM(CASE WHEN stage = 'POST_FILTER' THEN candidate_count END) 
-            - SUM(CASE WHEN stage = 'POST_SEM_REL_FILTER' THEN candidate_count END) as n_filtered
-    from stage_counts
-    group by mmxRequestUUID, query, queryDate
-)
-
-WITH rpc AS (
-    SELECT
-        response.mmxRequestUUID,
-        request.query AS query,
-        date(queryTime) as queryDate,
-        OrganicRequestMetadata.candidateSources AS candidateSources,
-        response.semanticRelevanceModelInfo.modelSetName as sem_rel_modelset_name,
-        response.semanticRelevanceScores AS semanticRelevanceScores
-    FROM `etsy-searchinfra-gke-prod-2.thrift_mmx_listingsv2search_search.rpc_logs_*`
-    WHERE queryTime BETWEEN TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 7 DAY) AND CURRENT_TIMESTAMP()
+    WHERE queryTime BETWEEN TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 30 DAY) AND CURRENT_TIMESTAMP()
     AND request.options.cacheBucketId LIKE 'live%'
     AND request.query <> ''
     AND request.options.csrOrganic
@@ -109,14 +54,15 @@ semrel_filtered_count as (
 )
 
 SELECT
-  percentiles[OFFSET(10)] AS p10,
-  percentiles[OFFSET(50)] AS median,
-  percentiles[OFFSET(90)] AS p90,
-  percentiles[OFFSET(95)] AS p95,
-  percentiles[OFFSET(99)] AS p99
+  percentiles[OFFSET(100)] AS p10,
+  percentiles[OFFSET(500)] AS median,
+  percentiles[OFFSET(900)] AS p90,
+  percentiles[OFFSET(950)] AS p95,
+  percentiles[OFFSET(990)] AS p99,
+  percentiles[OFFSET(999)] AS p999
 FROM (
   SELECT
-    APPROX_QUANTILES(n_filtered, 100) AS percentiles
+    APPROX_QUANTILES(n_filtered, 1000) AS percentiles
   FROM
     semrel_filtered_count
 )
