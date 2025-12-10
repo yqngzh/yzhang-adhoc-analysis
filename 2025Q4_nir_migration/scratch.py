27e9c62733bd5bd2e4b45784b1adae66a5954d3f
Yuqing Zhang <yzhang@etsy.com>
2025-12-10 09:30:03 -0800

    add scratch

--- 2025Q4_nir_migration/scratch.py
+++ 2025Q4_nir_migration/scratch.py
@@ -25,16 +25,21 @@ def load_raw_data_from_parquet_file(
 
 
 columns = None
-columns = [
-    "query_clientProvidedInfo.query.query#clientProvidedInfo.query.queryEn#clientProvidedInfo.query.queryCorrected#contextualInfo[name=target].docInfo.queryInfo.query#contextualInfo[name=target].id#fallback#word_2grams",
-    "listing_candidateInfo.docInfo.listingInfo.verticaListings.tags#word_2grams",
-]
+# columns = [
+#     "query_clientProvidedInfo.query.query#clientProvidedInfo.query.queryEn#clientProvidedInfo.query.queryCorrected#contextualInfo[name=target].docInfo.queryInfo.query#contextualInfo[name=target].id#fallback#word_2grams",
+#     "listing_candidateInfo.docInfo.listingInfo.verticaListings.tags#word_2grams",
+# ]
+
 # fpath = "gs://training-dev-search-data-jtzn/user/ci/preprocess-test-neural-ir-dedupe-combine-randomize-slim-yzhang/_split=train/30_fef2d51d7f8b4736936d86e743a2ecf8_000000_000000-0.parquet"
 # fpath = "gs://etldata-prod-adhoc-data-hkwv8r/data/shared/neural_ir/all/sample_for_testing/negatives/_DATE=2025-11-01/part-02402-8f7ceeb5-858f-421e-a698-a4e7d24b77b2-c000.gz.parquet"
-fpath = "gs://etldata-prod-adhoc-data-hkwv8r/data/shared/neural_ir/all/sample_for_testing/negatives/_DATE=2025-11-01/part-14840-a6d41b5f-1556-41b4-a0c1-c11bd349c419-c000.gz.parquet"
+# fpath = "gs://etldata-prod-adhoc-data-hkwv8r/data/shared/neural_ir/all/sample_for_testing/negatives/_DATE=2025-11-01/part-14840-a6d41b5f-1556-41b4-a0c1-c11bd349c419-c000.gz.parquet"
+# fpath = "gs://etldata-prod-search-ranking-data-hkwv8r/data/shared/neural_ir/all/ListingFeatures/nir_flat_pos/v2/query_pipeline_web_organic/tight_purchase/_DATE=2025-12-09/part-00199-ff0d2ae3-18db-49c9-9767-9304154f7f9b-c000.gz.parquet"
+# fpath = "gs://etldata-prod-search-ranking-data-hkwv8r/data/shared/neural_ir/all/ListingFeatures/active_listings/v2/_DATE=2025-12-09/part-03499-25dd0037-e493-4bf8-aa90-53baa14a8270-c000.snappy.parquet"
+fpath = "gs://training-dev-search-data-jtzn/user/ci/preprocess-test-neural-ir-main-yzhang/_split=train/30_d9939ad684e9415eafda2f8a5a1628fb_000000_000000-0.parquet"
 
 data = load_raw_data_from_parquet_file(fpath, columns)
 first_request = data[0]
+first_request.keys()
 # len(first_request["query_requestUUID"]) # 6 (1+5)
 # len(first_request["query_clientProvidedInfo.user.userCountry"]) # 6 (1+5)
 # len(first_request["listing_candidateInfo.docInfo.listingInfo.verticaListings.taxonomyPath"]) # 6 (1+5)
